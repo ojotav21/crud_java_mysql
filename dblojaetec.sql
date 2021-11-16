778c2d73d320ce578559000c57f2f69e458bb5eb
Gustavo Pinto <64512499+gustavohpinto@users.noreply.github.com>
2021-11-16 12:06:24 -0300

    DB - OS

--- dblojaetec.sql
+++ dblojaetec.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Set-2021 às 19:28
--- Versão do servidor: 10.4.14-MariaDB
--- versão do PHP: 7.2.34
+-- Tempo de geração: 16-Nov-2021 às 16:04
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,8 @@ CREATE TABLE `clientes` (
 
 INSERT INTO `clientes` (`idcli`, `nomecli`, `endcli`, `fonecli`, `emailcli`) VALUES
 (1, 'JV', 'Rua Endereço Jv, 8, Cidade Jv - Brasil\r\n', '11222223333', 'jv@email.com'),
-(2, 'JV', 'Rua Endereço Jv, 8, Cidade Jv - Brasil\r\n', '11222223333', 'jv@email.com');
+(2, 'JV', 'Rua Endereço Jv, 8, Cidade Jv - Brasil\r\n', '11222223333', 'jv@email.com'),
+(3, 'gusta', 'sqdq', 'sdasd', 'sdfqd');
 
 -- --------------------------------------------------------
 
@@ -56,19 +57,22 @@ CREATE TABLE `ordem_servico` (
   `data_os` timestamp NOT NULL DEFAULT current_timestamp(),
   `equipamento` varchar(150) NOT NULL,
   `defeito` varchar(150) NOT NULL,
-  `servico` varchar(150) DEFAULT NULL,
-  `tecnico` varchar(30) DEFAULT NULL,
-  `valor` decimal(10,2) DEFAULT NULL,
-  `idcli` int(11) NOT NULL
+  `servico` varchar(150) NOT NULL,
+  `tecnico` varchar(30) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `idcli` int(11) NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `situacao` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `ordem_servico`
 --
 
-INSERT INTO `ordem_servico` (`os`, `data_os`, `equipamento`, `defeito`, `servico`, `tecnico`, `valor`, `idcli`) VALUES
-(1, '2021-07-31 15:25:42', 'desktop', 'memoria ram mal colocada', 'Manutenção e troca de componentes\r\n', 'Andershow', NULL, 1),
-(2, '2021-07-31 15:25:45', 'desktop', 'memoria ram mal colocada', 'Manutenção e troca de componentes\r\n', 'Andershow', NULL, 1);
+INSERT INTO `ordem_servico` (`os`, `data_os`, `equipamento`, `defeito`, `servico`, `tecnico`, `valor`, `idcli`, `tipo`, `situacao`) VALUES
+(1, '2021-07-31 15:25:42', 'desktop', 'memoria ram mal colocada', 'Manutenção e troca de componentes\r\n', 'Andershow', '0.00', 1, '', ''),
+(2, '2021-07-31 15:25:45', 'desktop', 'memoria ram mal colocada', 'Manutenção e troca de componentes\r\n', 'Andershow', '0.00', 1, '', ''),
+(3, '2021-11-16 15:02:14', 'Coração', 'Amar demais', 'Balotelli Romantica', 'Madu', '1000.00', 3, 'Orçamento', 'Aguardando Aprovação');
 
 -- --------------------------------------------------------
 
@@ -127,13 +131,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idcli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idcli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `ordem_servico`
 --
 ALTER TABLE `ordem_servico`
-  MODIFY `os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
